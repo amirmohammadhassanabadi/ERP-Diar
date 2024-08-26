870b1b9da503358e071111bcb4e33055624bb253
amirha <hasanabadi.am1380@gmail.com>
2024-08-26 15:28:17 +0330

    texting login page & stylesheet

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
-const app = express();
 require("dotenv").config();
-const {authRouter} = require("./routers/auth")
+const path = require("path");
+
+// importing routers
+const { authRouter } = require("./routers/auth");
+
+// creating app - server
+const app = express();
 
 const port = process.env.PORT || 5000;
+
+// static files - public
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 
 // Routers
-app.use("/auth", authRouter)
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-})
+  console.log(`server running on port ${port}`);
+});
