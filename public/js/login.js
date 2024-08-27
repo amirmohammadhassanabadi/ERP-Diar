90a6779b03eacea7c798a6e07e56281bc849c1d2
amirha <hasanabadi.am1380@gmail.com>
2024-08-27 13:21:07 +0330

    login page - activate front JS

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,14 @@
-// const loginWrapper = document.querySelector(".login-wrapper");
-// loginWrapper.addEventListener("click", (e) => {
-//   if (e.target.id == "showIcon") {
-//     if (e.target.parentElement.nextElementSibling.type == "text") {
-//       e.target.parentElement.nextElementSibling.type = "password";
-//       e.target.classList = "fa-solid fa-eye";
-//     } else {
-//       e.target.parentElement.nextElementSibling.type = "text";
-//       e.target.classList = "fa-solid fa-eye-slash";
-//     }
-//   }
-// });
+// DOM Variables
+const loginWrapper = document.querySelector(".login-wrapper");
 
-
+loginWrapper.addEventListener("click", (e) => {
+  if (e.target.id == "showIcon") {
+    if (e.target.parentElement.nextElementSibling.type == "text") {
+      e.target.parentElement.nextElementSibling.type = "password";
+      e.target.classList = "fa-solid fa-eye";
+    } else {
+      e.target.parentElement.nextElementSibling.type = "text";
+      e.target.classList = "fa-solid fa-eye-slash";
+    }
+  }
+});
