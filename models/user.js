392e753daa598f464e2826598169ca4790a21f1b
amirha <hasanabadi.am1380@gmail.com>
2024-09-01 12:08:59 +0330

    user model updated

--- models/user.js
+++ models/user.js
@@ -2,7 +2,20 @@ const mongoose = require("mongoose");
 
 const Schema = new mongoose.Schema({
     username: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    tasks: [
+        {
+            title: String,
+            time: Date,
+            description: String
+        }
+    ],
+    macaddress: String,
+    department: {
+        type: String,
+        required: true,
+        enum: ["it"]
+    }
 })
 
 const User = new mongoose.model("User", Schema);
