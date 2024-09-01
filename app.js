2d71b9c6ea9c681dec5379682e8cdc3db84f1114
amirha <hasanabadi.am1380@gmail.com>
2024-09-01 12:28:20 +0330

    mongoDB - express connected

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const path = require("path");
+const mongoose = require("mongoose");
 
 // importing routers
 const { authRouter } = require("./routers/auth");
@@ -18,6 +19,8 @@ app.set("view engine", "ejs");
 // Routers
 app.use("/auth", authRouter);
 
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
-});
+mongoose.connect("mongodb://localhost:27017/Diar-ERP").then(() => {
+  app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+  });
+})
