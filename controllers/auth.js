f5b9b9bc83d8c62025a82c2f47fcf1a1a6363960
amirha <hasanabadi.am1380@gmail.com>
2024-09-01 12:22:04 +0330

    addUser controller added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,19 @@
+const {User} = require("../models/user");
+
 exports.renderLoginPage = (req, res) => {
     res.render("auth/login");
+}
+
+exports.addUser = async (req, res) => {
+    const {username, password, department} = req.body;
+    let newUser = new User({
+        username: username,
+        password: password,
+        department: department 
+    })
+
+    newUser = await newUser.save();
+    res.json({
+        status: 200
+    })
 }
