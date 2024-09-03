749ddd6cc227d2a2818ee016b345d69363c36106
amirha <hasanabadi.am1380@gmail.com>
2024-09-03 10:21:19 +0330

    jwt added to controller login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { User } = require("../models/user");
 const bcrypt = require("bcrypt");
+const JWT = require("../middleware/JWT");
 
 exports.renderLoginPage = (req, res) => {
   res.render("auth/login");
@@ -14,5 +15,6 @@ exports.postLogin = async (req, res) => {
   const matched = bcrypt.compare(password, user.password);
   if(!matched) return res.json({status: 401, message: "username or password is wrong"});
 
+  req.cookie("access-token", JWT.createToken(user), { maxAge:  60 * 60 * 24 * 1000, httpOnly: true })
   res.json({status: 200, message: "successful"})
 };
--- middleware/JWT.js
+++ middleware/JWT.js
@@ -2,6 +2,6 @@ const {sign, verify} = require("jsonwebtoken");
 const path = require("path")
 require("dotenv").config({path: path.join(__dirname, "..", ".env")});
 
-exports.create = async (user) => {
+exports.createToken = async (user) => {
     return sign({id: user._id, username: user.username}, process.env.JWT_SECRET_KEY);
 }
