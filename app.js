4d090b05fe5677eae090b0d31f0785b92eb54261
sphr0 <sepehr.eb02@gmail.com>
2024-10-05 16:44:15 +0330

    initial static front demo

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
-require("dotenv").config();
-const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+require('dotenv').config();
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 // importing routers
-const { authRouter } = require("./routers/auth");
+const { authRouter } = require('./routers/auth');
 
 // creating app - server
 const app = express();
@@ -14,9 +14,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // static files - public
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // Request Body handling
 app.use(express.json());
@@ -26,10 +26,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routers
-app.use("/auth", authRouter);
+app.use('/auth', authRouter);
 
-mongoose.connect("mongodb://127.0.0.1:27017/Diar-ERP").then(() => {
-  app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
+// mongoose.connect("mongodb://127.0.0.1:27017/Diar-ERP").then(() => {
+// });
--- routers/auth.js
+++ routers/auth.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
 const authRouter = express.Router();
-const authController = require("../controllers/auth");
-const JWT = require("../middleware/JWT");
+const authController = require('../controllers/auth');
+const JWT = require('../middleware/JWT');
 
-authRouter.get("/login", authController.renderLoginPage);
-authRouter.post("/postlogin", authController.postLogin);
-authRouter.get("/user/profile", JWT.validateToken, authController.renderProfilePage);
+authRouter.get('/login', authController.renderLoginPage);
+authRouter.post('/postlogin', authController.postLogin);
+// authRouter.get("/user/profile", JWT.validateToken, authController.renderProfilePage);
+authRouter.get('/user/profile', authController.renderProfilePage);
 
 module.exports = { authRouter };
