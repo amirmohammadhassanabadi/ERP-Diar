65e341abb2c3cdec6705222156e45f26abb5f18a
sphr0 <sepehr.eb02@gmail.com>
2024-10-09 15:29:28 +0330

    added popup close btn handler

--- public/js/tasksView.js
+++ public/js/tasksView.js
@@ -2,23 +2,29 @@ import View from '/js/view.js';
 
 class TaskView extends View {
   _parentEl = document.querySelector('.task--body-header');
+  _overlayEl = document.querySelector('.overlay');
   _errMessage;
   _message;
 
   addHandlerTaskPopupOpen() {
+    const overlayElement = this._overlayEl;
     this._parentEl.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--task-add');
       if (!btn) return;
-      const overlayEl = document.querySelector('.overlay');
-      overlayEl.classList.remove('hidden');
-      console.log(overlayEl);
+      overlayElement.classList.remove('hidden');
     });
   }
 
-  //   addHandlerTaskPopupClose() {
-  //     const parentElTmp = document.querySelector('.');
-  //     this._parentEl.addEventListener('click');
-  //   }
+  addHandlerTaskPopupClose() {
+    const overlayElement = this._overlayEl;
+    const parentElTask = document.querySelector('.btn--popup-cancel');
+    parentElTask.addEventListener('click', function (e) {
+      overlayElement.classList.add('hidden');
+      console.log('test');
+    });
+  }
+
+  //mtd: save to local
 }
 
 export default new TaskView();
