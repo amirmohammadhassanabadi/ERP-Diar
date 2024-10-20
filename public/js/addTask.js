a91803ebc07c3a7d6ff6ae2c49306198ff9c8466
amir <hasanabadi.am1380@gmail.com>
2024-10-20 12:15:35 +0330

    date input added

---
+++ public/js/addTask.js
@@ -0,0 +1,12 @@
+jalaliDatepicker.startWatch({
+    minDate: "attr",
+    maxDate: "attr",
+    time: true,
+});
+const popup__container = document.querySelector(".popup__container");
+// popup__container.addEventListener("click", (e) => {
+//   if(e.target.classList[0] == "submit__btn"){
+    
+//   }
+// });
+
