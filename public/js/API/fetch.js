a75ca0f1357758cb0b778dfe869d43ed4ea2ee41
amir <hasanabadi.am1380@gmail.com>
2024-10-26 12:56:07 +0330

    Feat: postAPI function added - client side

--- public/js/API/fetch.js
+++ public/js/API/fetch.js
@@ -3,6 +3,20 @@ const getAPI = async (url) => {
     return (await response.json());
 }
 
+const postAPI = async (url, payload) => {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            payload
+        })
+    })
+    return await response.json();
+}
+
 export {
-    getAPI
+    getAPI,
+    postAPI
 }
--- public/js/views/containerView.js
+++ public/js/views/containerView.js
@@ -1,73 +1,73 @@
-import view from '/js/views/view.js';
-import {getAPI} from "/js/API/fetch.js"
+import view from "/js/views/view.js";
+import { getAPI, postAPI } from "/js/API/fetch.js";
 
 const state = [
   {
-    taskId: '4',
-    taskTitle: 'task title 4',
+    taskId: "4",
+    taskTitle: "task title 4",
     taskStatus: 1,
-    referrals: ['SE', 'AB', 'CD'],
-    days: 5
+    referrals: ["SE", "AB", "CD"],
+    days: 5,
   },
   {
-    taskId: '1',
-    taskTitle: 'task title 1',
+    taskId: "1",
+    taskTitle: "task title 1",
     taskStatus: 1,
-    referrals: ['SE', 'AB', 'CD'],
-    days: 3
+    referrals: ["SE", "AB", "CD"],
+    days: 3,
   },
   {
-    taskId: '2',
-    taskTitle: 'task title 2',
+    taskId: "2",
+    taskTitle: "task title 2",
     taskStatus: 1,
-    referrals: ['SE', 'AB'],
-    days: 4
+    referrals: ["SE", "AB"],
+    days: 4,
   },
   {
-    taskId: '3',
-    taskTitle: 'task title 3',
+    taskId: "3",
+    taskTitle: "task title 3",
     taskStatus: 0,
-    referrals: ['SE'],
-    days: 5
-  }
+    referrals: ["SE"],
+    days: 5,
+  },
 ];
 
 const user = [
   {
-    id: '123456789',
-    userName: 'sepehrebrahimi',
-    fullName: 'سپهر ابراهیمی',
+    id: "123456789",
+    userName: "sepehrebrahimi",
+    fullName: "سپهر ابراهیمی",
     tasks: [],
     level: 2,
-    department: 'IT'
+    department: "IT",
   },
   {
-    id: '1123456789',
-    userName: 'amirmohammadhasanabadi',
-    fullName: 'امیر محمد حسن آبادی',
+    id: "1123456789",
+    userName: "amirmohammadhasanabadi",
+    fullName: "امیر محمد حسن آبادی",
     tasks: [],
     level: 2,
-    department: 'IT'
+    department: "IT",
   },
   {
-    id: '11123456789',
-    userName: 'mostafahosseini',
-    fullName: 'مصطفی حسینی',
+    id: "11123456789",
+    userName: "mostafahosseini",
+    fullName: "مصطفی حسینی",
     tasks: [],
     level: 3,
-    department: 'IT'
+    department: "IT",
   },
   {
-    id: '111123456789',
-    userName: 'kasragolirad',
-    fullName: 'کسری گلی راد',
+    id: "111123456789",
+    userName: "kasragolirad",
+    fullName: "کسری گلی راد",
     tasks: [],
     level: 2,
-    department: 'IT'
-  }
+    department: "IT",
+  },
 ];
 let taskCounter = 5;
-const parentEl = document.querySelector('.container');
+const parentEl = document.querySelector(".container");
 
 const renderContainerTasks = function () {
   // clear container
@@ -138,10 +138,10 @@ const generateMarkupDashboard = function () {
 // container event handlers
 
 const handleContainerNav = function (handler) {
-  const parentEl = document.querySelector('.c_header__nav');
-  parentEl.addEventListener('click', function (e) {
-    const navItem = e.target.closest('.nav__item');
-    if (!navItem || navItem.classList.contains('nav__item-active')) return;
+  const parentEl = document.querySelector(".c_header__nav");
+  parentEl.addEventListener("click", function (e) {
+    const navItem = e.target.closest(".nav__item");
+    if (!navItem || navItem.classList.contains("nav__item-active")) return;
 
     // 1. send appropriate task body to handler
     handler(navItem);
@@ -149,7 +149,7 @@ const handleContainerNav = function (handler) {
 };
 
 const navChangeTaskReload = function (status) {
-  const taskContainer = parentEl.querySelector('.task__container');
+  const taskContainer = parentEl.querySelector(".task__container");
 
   // clear container body
   view.clear(taskContainer);
@@ -158,7 +158,7 @@ const navChangeTaskReload = function (status) {
 };
 
 const navChangeAsignedTasks = function () {
-  const taskContainer = parentEl.querySelector('.task__container');
+  const taskContainer = parentEl.querySelector(".task__container");
 
   view.clear(taskContainer);
 
@@ -172,27 +172,27 @@ const navChangeAsignedTasks = function () {
 };
 
 const switchActiveNav = function (navItem) {
-  const parentEl = document.querySelector('.c_header__nav');
+  const parentEl = document.querySelector(".c_header__nav");
 
   // remove current active nav class
   parentEl
-    .querySelector('.nav__item-active')
-    ?.classList.remove('nav__item-active');
+    .querySelector(".nav__item-active")
+    ?.classList.remove("nav__item-active");
 
   // add active to target nav item
-  navItem.classList.add('nav__item-active');
+  navItem.classList.add("nav__item-active");
 };
 
 // gets called while no container child on screen BUG
 // should handle when it gets called in control so it doesn't get called on init()
 const handleTaskAddBtn = function () {
-  const overlayEl = document.querySelector('.overlay');
-  const parentEl = document.querySelector('.container__body');
+  const overlayEl = document.querySelector(".overlay");
+  const parentEl = document.querySelector(".container__body");
   try {
-    parentEl.addEventListener('click', function (e) {
-      const btn = e.target.closest('.task__btn-add');
+    parentEl.addEventListener("click", function (e) {
+      const btn = e.target.closest(".task__btn-add");
       if (!btn) return;
-      overlayEl.classList.remove('hidden');
+      overlayEl.classList.remove("hidden");
 
       // ASYNC => GET USERS REMINDER
 
@@ -204,48 +204,48 @@ const handleTaskAddBtn = function () {
 };
 
 const handleOverlayLayer = function () {
-  const overlayEl = document.querySelector('.overlay');
-  overlayEl.addEventListener('click', function (e) {
+  const overlayEl = document.querySelector(".overlay");
+  overlayEl.addEventListener("click", function (e) {
     const clicked = e.target;
     if (!(clicked === overlayEl)) return;
-    overlayEl.classList.add('hidden');
-    const popupList = document.querySelector('.popup__overlay');
+    overlayEl.classList.add("hidden");
+    const popupList = document.querySelector(".popup__overlay");
     console.log(popupList);
 
-    if (popupList && !popupList.classList.contains('hidden'))
-      popupList.classList.add('hidden');
+    if (popupList && !popupList.classList.contains("hidden"))
+      popupList.classList.add("hidden");
   });
 };
 
 const handleReferralsBtn = async function () {
-  const popupContainer = document.querySelector('.popup__container');
-  popupContainer.addEventListener('click', async function (e) {
-    const agentBtn = e.target.closest('.agent-btn');
+  const popupContainer = document.querySelector(".popup__container");
+  popupContainer.addEventListener("click", async function (e) {
+    const agentBtn = e.target.closest(".agent-btn");
     if (!agentBtn) return;
 
     // Fetch Agents Data
     const data = await getAPI("/tasks/agent");
-    
+
     handlePopupUserList();
   });
 };
 
 const handlePopupUserList = function () {
-  const popupUserList = document.querySelector('.popup__overlay');
-  if (!popupUserList?.classList.contains('hidden')) {
+  const popupUserList = document.querySelector(".popup__overlay");
+  if (!popupUserList?.classList.contains("hidden")) {
     view.clear(popupUserList);
-    popupUserList.classList.add('hidden');
+    popupUserList.classList.add("hidden");
   } else {
     const markupGen = () => `
     <div class="popup">
     <ul class="user__list">
-    ${user.map(generateUsersListMarkup).join('')}
+    ${user.map(generateUsersListMarkup).join("")}
     </ul>
     </div>
     `;
 
     view.clear(popupUserList);
-    popupUserList.classList.remove('hidden');
+    popupUserList.classList.remove("hidden");
 
     // LOAD SPINNER ON DIV
 
@@ -260,21 +260,21 @@ const generateUsersListMarkup = async function (user) {
     <li>
       <div class="user__name" data-user-id="${user.id}"> ${user.fullName} </div>
       <div class="initial"> ${user.fullName
-        .split(' ')
+        .split(" ")
         .slice(0, 2)
-        .map(word => word[0].toUpperCase())
-        .join(' ')} </div>
+        .map((word) => word[0].toUpperCase())
+        .join(" ")} </div>
     </li>
   `;
 };
 
 const handleSelectedReferral = function () {
-  const userList = document.querySelector('.user__list');
-  const agentBtn = document.querySelector('.agent-btn');
-  const userDisplay = document.querySelector('.user__display');
+  const userList = document.querySelector(".user__list");
+  const agentBtn = document.querySelector(".agent-btn");
+  const userDisplay = document.querySelector(".user__display");
 
-  userList.addEventListener('click', function (e) {
-    const userEl = e.target.closest('li');
+  userList.addEventListener("click", function (e) {
+    const userEl = e.target.closest("li");
     if (!userEl) return;
     const userId = userEl.firstElementChild.dataset.userId;
 
@@ -284,46 +284,46 @@ const handleSelectedReferral = function () {
     // render user to referral div
 
     // 1. add hidden to referral btn
-    agentBtn.classList.toggle('hidden');
+    agentBtn.classList.toggle("hidden");
 
     view.renderHTML(generateUserReferralMarkup.bind(null, userId), userDisplay);
 
     // 2. add ev listener for close btn
 
     // 3. add hidden to referral btn
-    userList.classList.toggle('hidden');
+    userList.classList.toggle("hidden");
 
     // 4. add border to user display
-    userDisplay.classList.add('user__border');
+    userDisplay.classList.add("user__border");
   });
 };
 
 const handleUserCloseBtn = function () {
-  const userDisplay = document.querySelector('.user__display');
-  const closeBtn = document.querySelector('.user__close__btn');
+  const userDisplay = document.querySelector(".user__display");
+  const closeBtn = document.querySelector(".user__close__btn");
 };
 
 const generateUserReferralMarkup = function (id) {
-  const targetUsr = user.find(usr => usr.id === id);
+  const targetUsr = user.find((usr) => usr.id === id);
   return `
    <div class="user__name" data-user-id="${targetUsr.id}"> ${
     targetUsr.fullName
   } </div>
    <div class="initial"> ${targetUsr.fullName
-     .split(' ')
+     .split(" ")
      .slice(0, 2)
-     .map(word => word[0].toUpperCase())
-     .join(' ')} </div>
+     .map((word) => word[0].toUpperCase())
+     .join(" ")} </div>
   `;
 };
 
 const handleCheckbox = function (handler) {
-  const parentEl = document.querySelector('.task__container');
-  parentEl.addEventListener('click', function (e) {
-    const checkbox = e.target.closest('.checkbox');
+  const parentEl = document.querySelector(".task__container");
+  parentEl.addEventListener("click", function (e) {
+    const checkbox = e.target.closest(".checkbox");
     if (!checkbox) return;
 
-    const targetTaskId = e.target.closest('.task').dataset.taskId;
+    const targetTaskId = e.target.closest(".task").dataset.taskId;
     handler(targetTaskId);
 
     // console.log('target task id: ', targetTaskId);
@@ -362,25 +362,25 @@ const handleCheckbox = function (handler) {
 // close and submit share parentEl => one ev listener for each REFACTOR
 
 const handlePopupClose = function () {
-  const overlayEl = document.querySelector('.overlay');
-  const parentEl = document.querySelector('.popup__downside');
+  const overlayEl = document.querySelector(".overlay");
+  const parentEl = document.querySelector(".popup__downside");
 
-  parentEl.addEventListener('click', function (e) {
-    const btn = e.target.closest('.cancel__btn');
+  parentEl.addEventListener("click", function (e) {
+    const btn = e.target.closest(".cancel__btn");
     if (!btn) return;
-    overlayEl.classList.add('hidden');
+    overlayEl.classList.add("hidden");
   });
 };
 
 const handlePopupSubmit = function () {
-  const btnParentEl = document.querySelector('.popup__downside');
-  const inputEl = document.querySelector('.title-input');
-  const overlayEl = document.querySelector('.overlay');
+  const btnParentEl = document.querySelector(".popup__downside");
+  const inputEl = document.querySelector(".title-input");
+  const overlayEl = document.querySelector(".overlay");
   // temporary day input
-  const timeInputEl = document.querySelector('.description__item');
+  const timeInputEl = document.querySelector(".description__item");
 
-  btnParentEl.addEventListener('click', function (e) {
-    const btn = e.target.closest('.submit__btn');
+  btnParentEl.addEventListener("click", async function (e) {
+    const btn = e.target.closest(".submit__btn");
     if (!btn) return;
     // func to check conditions
     if (!inputEl.value) {
@@ -389,28 +389,43 @@ const handlePopupSubmit = function () {
 
     // default day is set to 1
     let daysInput = Number(timeInputEl.value) ? Number(timeInputEl.value) : 1;
+
+    // =====================================
+    // payload: {
+    //   title: ,
+    //   description: ,
+    //   agents: ,
+    //   deadline:
+    // }
+    // POST task to DB REMINDER
+    // const postTask = await postAPI("/tasks/addtasks", {})
+    // =====================================
+    // /tasks/gettasks
+    // =====================================
+    
+
     // store in state
     state.ongoing.push({
       taskId: taskCounter,
       taskTitle: inputEl.value,
-      referrals: ['SE'],
-      days: daysInput
+      referrals: ["SE"],
+      days: daysInput,
     });
     taskCounter++;
 
     // close popup
-    overlayEl.classList.remove('hidden');
+    overlayEl.classList.remove("hidden");
 
     // show success message
 
     // re-render tasks list
-    const taskContainerEl = document.querySelector('.task__container');
-    taskContainerEl.innerHTML = '';
+    const taskContainerEl = document.querySelector(".task__container");
+    taskContainerEl.innerHTML = "";
     view.renderHTML(generateTaskContainer, taskContainerEl);
-    inputEl.value = '';
-    timeInputEl.value = '';
+    inputEl.value = "";
+    timeInputEl.value = "";
     persistTasks();
-    overlayEl.classList.add('hidden');
+    overlayEl.classList.add("hidden");
   });
 };
 
@@ -421,7 +436,7 @@ const generateTaskContainer = function (status) {
               return acc;
             }, 0)} مورد)</span>
           <ul class="task__list">
-          ${state.map(generateSingleTask.bind(null, status)).join('')}
+          ${state.map(generateSingleTask.bind(null, status)).join("")}
           </ul>
 
   `;
@@ -436,21 +451,21 @@ const generateSingleTask = function (status, task) {
     <li class="task" data-task-id="${task.taskId}">
     <div class="task__right">
       <input class="checkbox" type="checkbox" ${
-        status ? 'checked' : ''
+        status ? "checked" : ""
       } data-id=${task.taskId} />
       <span class="task-text">${task.taskTitle}</span>
     </div>
     <div class="task__left">
       <div class="assignedto">
         ${task.referrals
-          .map(person => {
+          .map((person) => {
             i++;
 
             return `<div class="initial ${
-              i == 1 ? '' : `initial-${i}`
+              i == 1 ? "" : `initial-${i}`
             }">${person}</div>`;
           })
-          .join('')}
+          .join("")}
       </div>
       <span class="deadline">${task.days} روز مانده</span>
     </div>
@@ -475,5 +490,5 @@ export default {
   handleCheckbox,
   renderContainerTasks,
   handleOverlayLayer,
-  handleReferralsBtn
+  handleReferralsBtn,
 };
