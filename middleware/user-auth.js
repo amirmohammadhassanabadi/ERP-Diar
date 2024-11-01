f51eaf5fac02ee80b1a2315a0e16687b08383276
amir <hasanabadi.am1380@gmail.com>
2024-11-01 17:26:46 +0330

    Feat: sign up access fixed

--- middleware/user-auth.js
+++ middleware/user-auth.js
@@ -2,11 +2,12 @@ const {accessUsers} = require("../config/access/access-user");
 
 function signUpAccess(req, res, next) {
     const {level} = req.user;
+    
     if (!level) {
-        return res.redirect("/auth/user/profile");
+        return res.redirect("/");
     }
     if (!accessUsers.includes(level)) {
-        return res.redirect("/auth/user/profile");
+        return res.redirect("/");
     }
     
     next();
