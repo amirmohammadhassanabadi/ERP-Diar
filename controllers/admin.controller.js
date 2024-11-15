26c15aee54ded551ed16673e1443349a2b1f3669
amir <hasanabadi.am1380@gmail.com>
2024-11-15 19:13:38 +0330

    Feat: get users controller done

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,7 +1,25 @@
+const {User} = require("../models/user");
+
 exports.rederAdminPage = (req, res) => {
     res.render("admin/profile");
 }
 
 exports.getAllUsers = async (req, res) => {
-    
+ try {
+    const users = await User.find();
+
+    if (!users) {
+        throw new Error("something wrong happend");
+    }
+
+    res.status(200).json({
+        statusCode: 200,
+        data: users
+    })
+ } catch (error) {
+    res.status(500).json({
+        statusCode: 500,
+        message: `internal error - ${error.message}`
+    })
+ }   
 }
