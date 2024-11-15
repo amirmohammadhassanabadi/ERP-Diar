272fbca19e3373f1f01902e8dd89cfb18471412f
amir <hasanabadi.am1380@gmail.com>
2024-11-15 19:05:28 +0330

    Feat: user add button done

--- public/js/admin/menu.js
+++ public/js/admin/menu.js
@@ -1,3 +1,5 @@
+import { getAPI, postAPI } from "/js/API/fetch.js";
+
 function containerReseter() {
     document.querySelector(".container").innerHTML = "";
 }
@@ -5,6 +7,13 @@ function containerReseter() {
 document.querySelector(".menu").addEventListener("click", e => {
     if (e.target.classList.contains("user-setting") || e.target.parent.classList.contains("user-setting")) {
         containerReseter();
-        
+        let userCont = `
+            <div class="user-add-wrapper">
+              <a href="/auth/signup">افزودن کاربر</a>
+            </div>
+        `;
+
+
+        document.querySelector(".container").innerHTML = userCont;
     }
 })
